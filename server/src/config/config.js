8b4ed8762a169c44078a95fd2822c9dec38072d7
nayebare <mnayebare@rwanda.cmu.edu>
2018-10-03 05:21:36 +0300

    completed the user email signup

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -12,11 +12,12 @@ export const config = {
   },
   test:
   {
-    user: process.env.testUserName,
-    password: process.env.testPassword,
-    database: 'postgres',
-    host: '127.0.0.1',
-    port: process.env.conPort,
+    user: 'nrukmxauomxysi',
+    database: 'd4euko1ikv7r23',
+    host: 'ec2-23-21-171-249.compute-1.amazonaws.com',
+    password: 'c49acfd496fb5306a94aeecb8b82848739b05785accc835150ac465eeb112b7d',
+    port: '5432',
+    ssl: true
   },
   production: {
     use_env_variable: 'DATABASE_URL'
